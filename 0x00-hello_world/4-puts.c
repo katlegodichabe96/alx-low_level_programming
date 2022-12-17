582832ab3f8184c6a0e971b3ea59a48149fd7406
katlegodichabe96 <katlegodichabe96@gmail.com>
2022-12-16 23:46:54 -0800

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: Always o (success)
+*/
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
